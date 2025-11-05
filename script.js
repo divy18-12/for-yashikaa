245e83dcbb4c4feacc3b7706aa82027abf99f988
divy18-12 <divyansh.kriss@gmail.com>
2025-11-05 21:58:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+
 const garden = document.getElementById("garden");
 const message = document.getElementById("message");
 const tap = document.getElementById("tapScreen");
 const music = document.getElementById("bgMusic");
 
-// Create flowers
 for(let i = 0; i < 12; i++){
   const flower = document.createElement("div");
   flower.classList.add("flower");
@@ -12,7 +12,7 @@ for(let i = 0; i < 12; i++){
   stem.classList.add("stem");
   flower.appendChild(stem);
 
-  for(let p = 0; p < 4; p++){
+  for(let p = 0; p < 5; p++){
     const petal = document.createElement("div");
     petal.classList.add("petal");
     flower.appendChild(petal);
@@ -33,3 +33,14 @@ tap.addEventListener("click", () => {
     message.style.transform = "translateY(0)";
   }, 3500);
 });
+
+function createSparkle() {
+  const sparkle = document.createElement("div");
+  sparkle.classList.add("sparkle");
+  sparkle.style.left = Math.random() * window.innerWidth + "px";
+  sparkle.style.top = Math.random() * window.innerHeight + "px";
+  document.body.appendChild(sparkle);
+  setTimeout(() => sparkle.remove(), 3000);
+}
+
+setInterval(createSparkle, 900);
